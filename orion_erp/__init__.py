82f39a26d7cfffa43965b3d3fb3bedc2c5f4d872
MahakMujawadiya08 <mahak@8848digital.com>
2026-05-22 23:54:37 +0530

    Revert "Init file fix"

--- orion_erp/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = "0.0.1"
-
-# Monkey patch
-from frappe.core.doctype.file.file import File
-from orion_erp.override.files import is_remote_file,get_full_path
-File.is_remote_file = property(is_remote_file)
-File.get_full_path = get_full_path
