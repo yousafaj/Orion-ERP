1455a1d9a782ce524bd16cd8bfff5f8f70b64388
yousafaj <yousafaj@gmail.com>
2026-06-02 18:44:56 +0400

    Add tests for auto-expiration of LOAs

--- orion_erp/orion_erp/doctype/loa/test_loa.py
+++ orion_erp/orion_erp/doctype/loa/test_loa.py
@@ -1,9 +1,29 @@
-# Copyright (c) 2025, osama.ahmed@deliverydevs.com and Contributors
+# Copyright (c) 2026, Orion ERP and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
+from frappe.utils import add_days, nowdate
+
+from orion_erp.orion_erp.doctype.loa.loa import auto_expire_loas
+from orion_erp.tests.fixtures import create_loa
 
 
 class TestLOA(FrappeTestCase):
-	pass
+	def test_auto_expire_marks_past_dated_active_loa_expired(self):
+		loa = create_loa(expiry_date=add_days(nowdate(), -1))  # submitted + Active + expired
+		self.assertEqual(loa.loa_status, "Active")
+
+		auto_expire_loas()
+
+		loa.reload()
+		self.assertEqual(loa.loa_status, "Expired")
+		self.assertEqual(loa.active, 0)
+
+	def test_auto_expire_leaves_future_dated_loa_active(self):
+		loa = create_loa(expiry_date=add_days(nowdate(), 30))
+
+		auto_expire_loas()
+
+		loa.reload()
+		self.assertEqual(loa.loa_status, "Active")
