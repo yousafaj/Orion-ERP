e5bd6f8ecea26c731058386f91491c87c1c75af0
Mahak Mujawadiya08 <mahak@8848digital.com>
2026-06-30 08:21:22 +0000

    feat: Rejoining Form customization and sandwich leave changes

--- orion_erp/hooks.py
+++ orion_erp/hooks.py
@@ -50,12 +50,14 @@ doctype_js = {
     "Job Offer":"public/js/job_offer.js",
     "Driver": "public/js/driver.js",
     "Quotation": "public/js/quotation.js",
-    "Leave Allocation": "public/js/leave_allocation.js"
+    "Leave Allocation": "public/js/leave_allocation.js",
+    "Rejoining Form": "public/js/rejoining_form.js"
     }
 
 # app_include_css = "/assets/orion_erp/css/listview.css"
 doctype_list_js = {"Employee": "public/js/employee_list.js",
-                   "Leave Application": "public/js/leave_application_list.js"}
+                   "Leave Application": "public/js/leave_application_list.js",
+                   "Rejoining Form": "public/js/rejoining_form_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
@@ -107,6 +109,12 @@ fixtures = [
     {
         "doctype": "Role",
         "filters": [["name", "in", ["PRO"]]]
+    },
+    {
+        "doctype": "Weekday",
+        "filters": [
+            ["name", "in", ["Saturday", "Sunday"]]
+        ]
     }
 ]
 
@@ -164,7 +172,9 @@ permission_query_conditions = {
     "Additional Salary": "orion_erp.orion_erp.permission_query.additonal_salary.get_additional_salary_permission_query",
 	"Salary Structure Assignment": "orion_erp.orion_erp.permission_query.salary_structure_assignment.get_ssa_permission_query",
     "Leave Application":
-    "orion_erp.orion_erp.permission_query.leave_application.leave_application_query"
+    "orion_erp.orion_erp.permission_query.leave_application.leave_application_query",
+    "Rejoining Form":
+    "orion_erp.orion_erp.permission_query.rejoining_form.rejoining_form_query"
 }
 #
 # has_permission = {
@@ -211,6 +221,21 @@ doc_events = {
         ],
         "on_cancel":[
             "orion_erp.orion_erp.validations.leave_application.on_cancel_leave_application"
+        ]
+    },
+    "Rejoining Form":{
+        "validate":[
+            "orion_erp.orion_erp.validations.rejoining_form.validate_rejoining_approval",
+            "orion_erp.orion_erp.validations.rejoining_form.reset_status_on_amend"
+        ],
+        "on_update":[
+            "orion_erp.orion_erp.validations.rejoining_form.handle_rejoining_approval"
+        ],
+        "on_submit":[
+            "orion_erp.orion_erp.validations.rejoining_form.on_submit_rejoining_form"
+        ],
+        "on_cancel":[
+            "orion_erp.orion_erp.validations.rejoining_form.on_cancel_rejoining_form"
         ]
     },
     "Salary Structure Assignment":{
--- orion_erp/orion_erp/doctype/leave_declaration/leave_declaration.js
+++ orion_erp/orion_erp/doctype/leave_declaration/leave_declaration.js
@@ -3,61 +3,70 @@
 
 frappe.ui.form.on("LEAVE DECLARATION", {
 
-    // Fetch the passport number from Employee record
     employee: function(frm) {
-
         if (!frm.doc.employee) return;
 
         frappe.call({
             method: "orion_erp.orion_erp.doctype.leave_declaration.leave_declaration.get_passport_number",
-            args: {
-                employee: frm.doc.employee
-            },
+            args: { employee: frm.doc.employee },
             callback: function(r) {
-
-                // Set passport number if returned from server
                 if (r.message) {
                     frm.set_value("passport_number", r.message);
                 }
             }
         });
-
     },
 
-    // Apply filter on Employee field to show only Active employees
     setup: function(frm) {
         frm.set_query("employee", function() {
             return {
-                filters: {
-                    status: "Active"
-                }
+                filters: { status: "Active" }
+            };
+        });
+
+        frm.set_query("leave_type", function() {
+            return {
+                filters: { docstatus: 0 }
             };
         });
     },
 
-    // Recalculate leave days when start date changes
     leave_start_date: function(frm) {
         calculate_leave_days(frm);
     },
 
-    // Recalculate leave days when end date changes
     leave_end_date: function(frm) {
         calculate_leave_days(frm);
+    },
+
+    rejoining_date: function(frm) {
+        if (frm.doc.leave_end_date && frm.doc.rejoining_date) {
+            let end = frappe.datetime.str_to_obj(frm.doc.leave_end_date);
+            let rj = frappe.datetime.str_to_obj(frm.doc.rejoining_date);
+            if (frappe.datetime.get_day_diff(rj, end) < 0) {
+                frappe.msgprint({
+                    title: __("Early Return"),
+                    indicator: "orange",
+                    message: __("Employee is returning early. Leave application will be adjusted accordingly."),
+                    alert: true
+                });
+            } else if (frappe.datetime.get_day_diff(rj, end) > 0) {
+                frappe.msgprint({
+                    title: __("Extended Leave"),
+                    indicator: "orange",
+                    message: __("Employee is returning late. Extended leave application will be created."),
+                    alert: true
+                });
+            }
+        }
     }
 });
 
-
-// Calculate total leave days between start and end date
 function calculate_leave_days(frm) {
-
     if (frm.doc.leave_start_date && frm.doc.leave_end_date) {
-
         let start = frappe.datetime.str_to_obj(frm.doc.leave_start_date);
         let end = frappe.datetime.str_to_obj(frm.doc.leave_end_date);
-
         let diff = frappe.datetime.get_day_diff(end, start) + 1;
-
-        // Set calculated leave days in the field
         frm.set_value("leave_days", diff);
     }
 }
